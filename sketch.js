79f1b18cb982fac27494df1456199f28ab612b5d
huumpf <nico@windwalk.de>
2019-10-12 19:47:31 +0200

    Working multiple circles

--- sketch.js
+++ sketch.js
@@ -7,41 +7,45 @@ var points = [];
 
 const CFG = {
 
-  point_count: 40,
-  radius: 350,
+  point_count: 20,
+  radius: 200,
   noise_inc: 0.005,
-  noise_amp: 300
+  noise_amp: 400
 
 }
+
 
 function setup() {
 
   canvas = createCanvas(window.innerWidth, window.innerHeight);
   // noLoop();
   background( 70 );
+  smooth();
 
 }
+
 
 function draw() {
 
   background(10, 15);
-  points = makePoints();
-  drawPoints();
+  
+  for (let i = 0; i < 15; i++) {
+    points.push( makePoints(i) );
+    drawPoints();
+  }
   noise_time += CFG.noise_inc;
 }
 
-function makePoints() {
+
+function makePoints( mod ) {
 
   this.points = [];
-
   for (let i = 0; i < CFG.point_count; i++) {
     let pt = new p5.Vector.fromAngle( TWO_PI / CFG.point_count * i, CFG.radius);
     pt.add( new p5.Vector(width/2, height/2) );
-
-    let noise_displace_x = ( noise(pt.y * CFG.noise_inc, noise_time) -0.5 )* CFG.noise_amp;
-    let noise_displace_y = ( noise(pt.x * CFG.noise_inc, noise_time) -0.5 )* CFG.noise_amp;
+    let noise_displace_x = ( noise(pt.y * CFG.noise_inc, noise_time + mod/10) -0.5 ) * CFG.noise_amp;
+    let noise_displace_y = ( noise(pt.x * CFG.noise_inc, noise_time + mod/10) -0.5 ) * CFG.noise_amp;
     pt.add( new p5.Vector( noise_displace_x, noise_displace_y ));
-    
     this.points.push( pt );
   }
 
@@ -49,15 +53,18 @@ function makePoints() {
 
 }
 
+
 function drawPoints() {
-  stroke(255, 40);
+  stroke(255, 3);
   strokeWeight(1);
   noFill();
-  beginShape();
-  for (let i = 0; i < points.length; i++) {
-    curveVertex(points[i].x, points[i].y);
-  }
-  endShape(CLOSE);
+  points.forEach(pts => {
+    beginShape();
+    for (let i = 0; i < points.length; i++) {
+      curveVertex(points[i].x, points[i].y);
+    }
+    endShape(CLOSE);
+  });
 }
 
 function keyTyped() {
