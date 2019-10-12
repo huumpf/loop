e7de6778b9dff1a6630515ccafe37b29147ef77f
huumpf <nico@windwalk.de>
2019-10-12 17:43:35 +0200

    Improved first version

--- sketch.js
+++ sketch.js
@@ -8,38 +8,45 @@ var points = [];
 const CFG = {
 
   point_count: 40,
-  radius: 400,
-  noise_inc: 0.01,
-  noise_amp: 500
+  radius: 350,
+  noise_inc: 0.005,
+  noise_amp: 300
 
 }
 
 function setup() {
+
   canvas = createCanvas(window.innerWidth, window.innerHeight);
   // noLoop();
+  background( 70 );
+
 }
 
 function draw() {
-  background(30, 15);
-  makePoints();
+
+  background(10, 15);
+  points = makePoints();
   drawPoints();
   noise_time += CFG.noise_inc;
 }
 
 function makePoints() {
 
-  points = [];
+  this.points = [];
 
   for (let i = 0; i < CFG.point_count; i++) {
-    // console.log("point created");
     let pt = new p5.Vector.fromAngle( TWO_PI / CFG.point_count * i, CFG.radius);
     pt.add( new p5.Vector(width/2, height/2) );
-    let noise_displace_x = ( noise(pt.x * CFG.noise_inc, noise_time) -0.5 )* CFG.noise_amp;
-    let noise_displace_y = ( noise(pt.y * CFG.noise_inc, noise_time) -0.5 )* CFG.noise_amp;
-    // console.log( CFG.noise_amp );
+
+    let noise_displace_x = ( noise(pt.y * CFG.noise_inc, noise_time) -0.5 )* CFG.noise_amp;
+    let noise_displace_y = ( noise(pt.x * CFG.noise_inc, noise_time) -0.5 )* CFG.noise_amp;
     pt.add( new p5.Vector( noise_displace_x, noise_displace_y ));
-    points.push( pt );
+    
+    this.points.push( pt );
   }
+
+  return this.points;
+
 }
 
 function drawPoints() {
