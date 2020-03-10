6100f1614922e8edf7844a711e77aae4b44aeb11
huumpf <nico@windwalk.de>
2020-03-11 00:41:24 +0100

    Exchanged animation, Finished intro

--- sketch.js
+++ sketch.js
@@ -5,68 +5,74 @@ var noise_time = 0;
 
 var points = [];
 
-const CFG = {
+let CFG = {
 
+  lines_count: 6,
   point_count: 12,
-  radius: 300,
-  noise_inc: 0.005,
-  noise_amp: 300
+  noise_inc: 0.004,
+  noise_amp: 1000, // set in setup
+  time_divider: 10
 
 }
 
 
 function setup() {
 
-  canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas = createCanvas(window.innerWidth, window.innerHeight*4);
   canvas.parent("background_anim");
-  noLoop();
-  background( 40 );
+  background( 70 );
   smooth();
-
+  // noLoop();
 }
 
 
 function draw() {
 
-  background(20, 20);
+  background(0, 15);
   
-  for (let i = 0; i < 10; i++) {
-    points.push( makePoints(i) );
+  points = [];
+  for (let i = 0; i < CFG.lines_count; i++) {
+    let pts = makePoints(i)
+    points.push( pts );
+    push();
+    translate( width/2, -CFG.noise_amp/3 );
     drawPoints();
+    pop();
   }
+  
   noise_time += CFG.noise_inc;
 }
 
 
 function makePoints( mod ) {
 
-  this.points = [];
+  let _points = [];
+
   for (let i = 0; i < CFG.point_count; i++) {
-    let pt = new p5.Vector.fromAngle( TWO_PI / CFG.point_count * i, CFG.radius);
-    pt.add( new p5.Vector(width/2, height/2) );
-    let noise_displace_x = ( noise(pt.y * CFG.noise_inc, noise_time + mod/10) -0.5 ) * CFG.noise_amp;
-    let noise_displace_y = ( noise(pt.x * CFG.noise_inc, noise_time + mod/10) -0.5 ) * CFG.noise_amp;
+    let pt = new p5.Vector(0, i * height/CFG.point_count);
+    let noise_displace_x = ( noise(pt.y * CFG.noise_inc, noise_time + mod/CFG.time_divider) -0.5 ) * CFG.noise_amp;
+    let noise_displace_y = ( noise(pt.x * CFG.noise_inc, noise_time + mod/CFG.time_divider) -0.5 ) * CFG.noise_amp;
     pt.add( new p5.Vector( noise_displace_x, noise_displace_y ));
-    this.points.push( pt );
+    _points.push( pt );
   }
 
-  return this.points;
+  return _points;
 
 }
 
-
 function drawPoints() {
-  stroke(255, 3);
+  stroke(255, 8);
   strokeWeight(1);
   noFill();
   points.forEach(pts => {
     beginShape();
-    curveVertex(points[points.length-1].x, points[points.length-1].y);
-    for (let i = 0; i < points.length; i++) {
-      curveVertex(points[i].x, points[i].y);
+    curveVertex(pts[0].x, pts[0].y);
+    for (let i = 0; i < pts.length; i++) {
+      curveVertex(pts[i].x, pts[i].y);
     }
-    curveVertex(points[0].x, points[0].y);
-    curveVertex(points[1].x, points[1].y);
+    // curveVertex(pts[pts.length-1].x, pts[pts.length-1].y);
+    // curveVertex(pts[0].x, pts[0].y);
+    // curveVertex(pts[1].x, pts[1].y);
     endShape();
   });
 }
